49938a63deab2f7ca5d3ef8a53efd57a83768695
youx <carla.mussolini@youxgroup.com.br>
2023-03-30 09:48:58 -0300

    Subindo modificações

--- paginainicial.js
+++ paginainicial.js
@@ -1,48 +1,48 @@
 const redirencionar = () => {
   window.location.href = "cadastro.html";
 };
-
 const tabela = () => {
+  // usuario.push({
+  //   nome: nome.value,
+  //   email: email.value,
+  //   data: data.value,
+  //   cargo: select_do_cargo.value,
+  // });
   window.location.href = "tabela.html";
 };
 
-const cadastrar = document.getElementById("enviar");
+const enviar = document.getElementById("enviar");
 const nome = document.getElementById("nome");
 const email = document.getElementById("email");
 const data = document.getElementById("data");
 const select = document.getElementById("select");
 const select_do_cargo = document.getElementById("select_do_cargo");
+let usuario = [];
 
-cadastrar.addEventListener("click", function (e) {
-  e.preventDefault();
-  console.log("nome", nome.value);
-  console.log("email", email.value);
-  console.log("data", data.value);
-  console.log("cargo", select_do_cargo.value);
-});
+console.log(usuario);
 
-function executar(nome, email, data, cargo) {
-  var tb = document.getElementById("tabela");
-  var quantidadelinhas = tb.rows;
-  var linha = tb.insertRow(quantidadelinhas);
+// enviar.addEventListener("click", function (e) {
+//   // e.preventDefault();
 
-  var cellcodigo = linha.insertCell(0);
-  var cellcodigoNome = linha.insertCell(1);
-  var cellcodigoEmail = linha.insertCell(2);
-  var cellcodigoData = linha.insertCell(3);
-  var cellcodigoCargo = linha.insertCell(4);
-
-  cellcodigo.innerHTML = quantidadelinhas;
-  cellcodigoNome.innerHTML = nome;
-  cellcodigoEmail.innerHTML = email;
-  cellcodigoData.innerHTML = data;
-  cellcodigoCargo.innerHTML = cargo;
-}
-
-// let select_do_cargo = document.querySelector("#select_do_cargo");
-// let cadastrar = document.querySelector("#cadastrar");
-// cadastrar.addEventListener("click", () => {
 // });
+
+// enviar(nome, email, data, cargo) {
+//   var tb = document.getElementById("tabela");
+//   var quantidadelinhas = tb.rows;
+//   var linha = tb.insertRow(quantidadelinhas);
+
+//   var cellcodigo = linha.insertCell(0);
+//   var cellcodigoNome = linha.insertCell(1);
+//   var cellcodigoEmail = linha.insertCell(2);
+//   var cellcodigoData = linha.insertCell(3);
+//   var cellcodigoCargo = linha.insertCell(4);
+
+//   cellcodigo.innerHTML = quantidadelinhas;
+//   cellcodigoNome.innerHTML = nome;
+//   cellcodigoEmail.innerHTML = email;
+//   cellcodigoData.innerHTML = data;
+//   cellcodigoCargo.innerHTML = cargo;
+// }
 
 // const select = document.getElementById("select");
 // for (var i = 1; i <= 31; i++) {
